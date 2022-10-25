17f652859e67c3e53085e55f7c3cac34517a91b9
Aleksandra <aleksandra.hinc00@gmail.com>
2022-10-25 13:02:18 +0200

    zmiany 22

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         int liczba=26;
-        System.out.println("Hello Ola " + "mam" + liczba + "lat");
+        System.out.println("Hello Ola" + "mam" + liczba + "lat");
     }
 }
