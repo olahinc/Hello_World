e153c2d9ac3c1a8997950df473dd282056e5004d
Aleksandra <aleksandra.hinc00@gmail.com>
2022-10-25 13:19:26 +0200

    Dodawanie wartości

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int liczba=26;
-        System.out.println("Hello Ola " + "mam " + liczba + "  lat");
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Podaj pierwszą wartość");
+        int number1=sc.nextInt();
+        System.out.print("Podaj drugą wartość");
+        int number2=sc.nextInt();
+        System.out.print("Łączny wynik to " + (number1 + number2));
     }
 }
