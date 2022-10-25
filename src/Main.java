b2d7f0ed237158d7089454add91e00832d9c95ce
Aleksandra <aleksandra.hinc00@gmail.com>
2022-10-25 13:27:48 +0200

    Imię  wiek  komentarz

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Podaj pierwszą wartość");
+        System.out.print("Jak masz na imię?");
+        String imie=sc.nextLine();
+        System.out.print("Ile masz lat?");
         int number1=sc.nextInt();
-        System.out.print("Podaj drugą wartość");
-        int number2=sc.nextInt();
-        System.out.print("Łączny wynik to " + (number1 + number2));
+        System.out.print("Cześć" + imie + " miło Cię poznać :)");
     }
 }
